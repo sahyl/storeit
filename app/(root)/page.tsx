390d2d8e615e601e8fce633f857ce72730dcf5fd
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-12-09 15:36:59 +0530

    change1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -61,7 +61,7 @@ const Dashboard = async () => {
 
       {/* Recent files uploaded */}
       <section className="dashboard-recent-files">
-        <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
+        <h2 className="h3 xl:h2 text-light-100">Recent files</h2>
         {files.documents.length > 0 ? (
           <ul className="mt-5 flex flex-col gap-5">
             {files.documents.map((file: Models.Document) => (
